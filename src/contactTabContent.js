c5538d8c69f8ff6a268825db378421e7d8718646
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-11-30 21:29:21 +0300

    Add and style contact tab content

--- src/contactTabContent.js
+++ src/contactTabContent.js
@@ -1,10 +1,15 @@
-// import { headLine } from "./headLine";
 import "./css/tab-content.css";
+import contactItem from "./contactItem";
 
 const contactTab = () => {
   const tab = document.createElement("div");
   tab.classList.add("tab-content");
-  tab.innerText = "This is the contact tab";
+  tab.appendChild(
+    contactItem("fa-home", "62A Luthili Avenue, Upper Kololo, Kampala Uganda")
+  );
+  tab.appendChild(contactItem("fa-phone", "0-800-JYUMMY, +256-772-111122"));
+  tab.appendChild(contactItem("fa-envelope", "justyummy@justyammy.food"));
+  tab.appendChild(contactItem("fa-whatsapp", "+256-772-111122"));
   return tab;
 };
 
